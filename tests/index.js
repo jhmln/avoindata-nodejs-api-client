4314096faaaae2acfa3a71892f237278f255de62
jhmln <229766646+jhmln@users.noreply.github.com>
2025-09-08 17:23:53 +0300

    Added the first integration tests

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,29 @@
+import assert from 'node:assert/strict';
+import { describe, it } from 'node:test';
 import { RegisteredNotices } from 'unofficial-avoindata-nodejs-client';
 
 const registeredNotices = new RegisteredNotices();
-// const notices = await registeredNotices.search();
-// console.log('Registered notices:', notices.totalResults);
+const notices = await registeredNotices.search();
 
-const descriptions = await registeredNotices.getCodeDescriptions(RegisteredNotices.CodeList.COMPANY_FORM, RegisteredNotices.Language.ENGLISH);
-console.log('Company form descriptions:', descriptions);
+describe('Search', () => {
+  const hasMandatoryProperties = hasProperty(notices, 'totalResults') && hasProperty(notices, 'companies');
+
+  it('Should have totalResults and companies property', () => {
+    assert.equal(hasMandatoryProperties, true);
+  });
+
+  it('Should return more than one company', () => {
+    assert.equal(notices.totalResults > 0, true);
+  });
+
+  const pages = Math.ceil(notices.totalResults / 50);
+
+  it('The last page should have less than or equal to 50 companies', async () => {
+    const lastPageNotices = await registeredNotices.search({ page: pages });
+    assert.equal(lastPageNotices.companies.length <= 50, true);
+  });
+});
+
+function hasProperty(value, property) {
+  return Object.prototype.hasOwnProperty.call(value, property);
+}
