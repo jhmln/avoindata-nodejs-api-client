8c8c97311de9da4d70ea5830402f8da564528d8b
jhmln <229766646+jhmln@users.noreply.github.com>
2025-09-13 11:29:43 +0300

    Added tests for the remaining methods

--- README.md
+++
@@ -1,6 +0,0 @@
-## Test set up
-
-Since the project is in early development and is going under a lot of changes, constantly updating the packages for testing would be tedious. As such I've opted to use the `npm link` to keep the test project up to date with the actual business logic.
-
-1. Move to the `src` folder and execute the `npm link` command.
-2. Move to the `tests` folder and execute the `npm link unofficial-avoindata-nodejs-client`.
--- tests/index.js
+++ tests/index.js
@@ -24,6 +24,94 @@ describe('Search', () => {
   });
 });
 
+const originalCompany = notices.companies[0];
+const comparisonCompany = await registeredNotices.getCompany(originalCompany.businessId.value);
+
+describe('Get company', () => {
+  it('Should throw an error if business ID is not valid', async () => {
+    await assert.rejects(async () => {
+      await registeredNotices.getCompany('invalid-id');
+    });
+  })
+
+  it('Should return the same company when searched by business ID', () => {
+    assert.deepEqual(originalCompany, comparisonCompany);
+  });
+});
+
+const companyWithNotices = notices.companies.find(company => 
+  hasProperty(company, 'publicNotices') && company.publicNotices.length > 0
+);
+
+if (companyWithNotices) {
+  const originalNotice = companyWithNotices.publicNotices[0];
+  const [recordYear, recordNumber] = originalNotice.recordNumber.split('/');
+
+  describe('Get public notice', () => {    
+    it('Should throw an error if record year is null', async () => {
+      await assert.rejects(async () => {
+        await registeredNotices.getPublicNotice(null, '123');
+      });
+    });
+
+    it('Should throw an error if record year is undefined', async () => {
+      await assert.rejects(async () => {
+        await registeredNotices.getPublicNotice(undefined, '123');
+      });
+    });
+
+    it('Should throw an error if record year is non-integer string', async () => {
+      await assert.rejects(async () => {
+        await registeredNotices.getPublicNotice('invalid-year', '123');
+      });
+    });
+
+    it('Should return a matching public notice', async () => {
+      const comparisonNotice = await registeredNotices.getPublicNotice(+recordYear, recordNumber);
+
+      for(let key in originalNotice) {
+        if(hasProperty(originalNotice, key)) {
+          assert.deepEqual(originalNotice[key], comparisonNotice[key]);
+        }
+      };
+    });
+  });
+}
+
+describe('Get code descriptions', () => {
+  it('Should throw an error if code list is invalid', async () => {
+    await assert.rejects(async () => {
+      await registeredNotices.getCodeDescriptions('INVALID_CODE', RegisteredNotices.Language.FINNISH);
+    }); 
+  });
+
+  it('Should throw an error if language is invalid', async () => {
+    await assert.rejects(async () => {
+      await registeredNotices.getCodeDescriptions(RegisteredNotices.CodeList.COMPANY_FORM, 'INVALID_LANGUAGE');
+    });
+  });
+
+  const codes = Object.values(RegisteredNotices.CodeList);
+  const languages = Object.values(RegisteredNotices.Language);
+
+  for (let codeIndex = 0; codeIndex < codes.length; codeIndex++) {
+    const code = codes[codeIndex];
+
+    for (let languageIndex = 0; languageIndex < languages.length; languageIndex++) {
+      const language = languages[languageIndex];
+
+      it(`Should return code descriptions for code ${code} in language ${language}`, async () => {
+        assert.ok(async () => await registeredNotices.getCodeDescriptions(code, language));
+        await delay(500);
+      });      
+    }
+  }
+});
+
 function hasProperty(value, property) {
   return Object.prototype.hasOwnProperty.call(value, property);
+}
+
+async function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
